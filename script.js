e1f2487116937df2ed6dbaeecb1653fa2e0ee558
Aleksandr <abyzovaleksandr5@gmail.com>
2023-05-28 15:53:55 +0500

    feat: add adaptive

--- script.js
+++ script.js
@@ -38,3 +38,81 @@ printLine();
 function getRandInt(maximum) {
     return Math.floor(Math.random() * Math.floor(maximum));
 }
+
+
+// Тема
+let flagTheme = false;
+
+function themeChange() {
+    const themeBtn = document.querySelector('.theme-btn');
+    themeBtn.addEventListener('click', () => {
+        flagTheme = !flagTheme;
+        themeBtn.classList.toggle('dark');
+        document.body.classList.toggle('night');
+    });
+}
+themeChange();
+
+// кнопка скролл
+const scrollToTopButton = document.getElementById("scrollToTop");
+const handleScroll = () => {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        scrollToTopButton.style.display = "block";
+    }
+    else {
+        scrollToTopButton.style.display = "none";
+    }
+};
+
+const scrollToTop = () => {
+    const distanceFromTop = document.documentElement.scrollTop || document.body.scrollTop;
+
+    if (distanceFromTop > 0) {
+        window.scrollTo(0, 0);
+    }
+};
+document.addEventListener("scroll", handleScroll);
+scrollToTopButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    scrollToTop();
+});
+
+//burger
+const burger = document.querySelector('.burger'),
+    burgerNav = document.querySelector('.nav'),
+    navLink = document.querySelectorAll('.nav-link'),
+    body = document.querySelector('body'),
+    // overlay = document.querySelector('.overlay'),
+    burgerBackground = document.querySelector('.burger-background');
+
+burger.addEventListener('click', function (e) {
+    // e.stopPropagation();
+    burger.classList.toggle('active');
+    burgerNav.classList.toggle('open');
+    body.classList.toggle('lock');
+    burgerBackground.classList.toggle('background-open')
+})
+
+//клик по ссылкам
+for (i = 0; i < navLink.length; i++) {
+    navLink[i].addEventListener('click', () => {
+        burger.classList.remove('active');
+        burgerNav.classList.remove('open');
+        body.classList.remove('lock');
+        burgerBackground.classList.remove('background-open')
+
+    })
+}
+
+// клик снаружи
+document.addEventListener('click', function (e) {
+    if (e.target === burgerBackground) {
+        burger.classList.remove('active');
+        burgerNav.classList.remove('open');
+        body.classList.remove('lock');
+        burgerBackground.classList.remove('background-open')
+    }
+})
+
+
+
